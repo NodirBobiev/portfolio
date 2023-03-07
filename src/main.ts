f8b8e4e7d9c75a78c7f8378f3561d983420696ac
Bobiev Nodir <bobievnodir@yandex-team.ru>
2023-03-07 12:50:24 +0300

    add RawJoke

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,20 @@ type Joke = {
     alt: string
 }
 
+interface RawJoke{
+    month: number;
+    year: number;
+    day: number;
+    num: number;
+    link?: string;
+    news?: string;
+    transcript: string;
+    safe_title: string;
+    alt: string;
+    img: string;
+    title: string;
+}
+
 fetch("https://fwd.innopolis.app/api/hw2?" + new URLSearchParams({email}))
 .then((response)=>response.text())
 .then(getXKCD)
@@ -15,12 +29,12 @@ fetch("https://fwd.innopolis.app/api/hw2?" + new URLSearchParams({email}))
 async function getXKCD(num: string){
     const response = await fetch("https://getxkcd.vercel.app/api/comic?" + new URLSearchParams({num}))
     
-    const data = await response.json();
+    const data : RawJoke = await response.json();
     const joke : Joke = {
         title : data.title,
         img : data.img,
         alt : data.alt,
-        date : new Date(Date.UTC(data['year'], data['month'], data['day'], 0, 0, 0)),
+        date : new Date(Date.UTC(data.year, data.month, data.day, 0, 0, 0)),
     }
     displayXKCD(joke);
 }

91b98b63e2224a73b56232c71ab639f39488f1b0
Bobiev Nodir <bobievnodir@yandex-team.ru>
2023-03-07 12:40:10 +0300

    4th lab is done

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+const email : string = "n.bobiev@innopolis.university";
+
+type Joke = { 
+    title: string, 
+    date: Date, 
+    img: string, 
+    alt: string
+}
+
+fetch("https://fwd.innopolis.app/api/hw2?" + new URLSearchParams({email}))
+.then((response)=>response.text())
+.then(getXKCD)
+
+
+async function getXKCD(num: string){
+    const response = await fetch("https://getxkcd.vercel.app/api/comic?" + new URLSearchParams({num}))
+    
+    const data = await response.json();
+    const joke : Joke = {
+        title : data.title,
+        img : data.img,
+        alt : data.alt,
+        date : new Date(Date.UTC(data['year'], data['month'], data['day'], 0, 0, 0)),
+    }
+    displayXKCD(joke);
+}
+    
+
+
+function displayXKCD(data: Joke){
+    console.log(data);
+
+    const trash = document.getElementById("trash") as HTMLDivElement;
+    
+    const title = document.createElement("h2") as HTMLHeadElement;
+    title.textContent = data["title"];
+    
+    const dateSpan = document.createElement("p") as HTMLParagraphElement;
+    dateSpan.textContent = data.date.toLocaleDateString("en-RU");
+
+    const img = document.createElement("img") as HTMLImageElement;
+    img.setAttribute("src", data["img"]);
+    img.setAttribute("alt", data["alt"]);
+
+
+    trash.appendChild(title);
+    trash.appendChild(dateSpan);
+    trash.appendChild(img);
+
+}
